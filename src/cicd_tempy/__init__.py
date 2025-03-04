36af2db38cf4a542adf9498ee65717a2142a5ab7
mhdmartini <mohamedmartini1991@gmail.com>
2025-03-03 22:42:22 -0500

    fix: move to commitizen

--- src/cicd_tempy/__init__.py
+++ src/cicd_tempy/__init__.py
@@ -1,3 +1,8 @@
 """Template for a simple Github workflow for a Python package built for distribution."""
 
-__version__ = '0.2.5'
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version('cicd_tempy')
+except importlib.metadata.PackageNotFoundError:
+    __version__ = '0.0.0'  # Default if package is not installed
